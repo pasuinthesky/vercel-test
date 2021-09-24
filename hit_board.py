48b767f73af59c9f4d610b27d396c525eba9236e
Glen Liu <glen.liu@cautiontape.ca>
2021-09-23 22:58:44 -0400

    VERCEL-1 Improved interface of baord class for better self-explaining naming.

--- hit_board.py
+++ hit_board.py
@@ -2,15 +2,15 @@ from datetime import datetime
 
 
 class board:
-    def __init__(self, num_of_bucket=300):
+    def __init__(self, sampling_interval=1):  # sampling interval in seconds
         self.begin = datetime.now().timestamp()
-        self.num_of_bucket = num_of_bucket
-        self.bucket_size = 300 / num_of_bucket
-        self.buckets = [0] * num_of_bucket
+        self.sampling_interval = sampling_interval
+        self.num_of_bucket = 300 / sampling_interval
+        self.buckets = [0] * self.num_of_bucket
 
     def hit(self):
         seq = int((datetime.now().timestamp() - self.begin) /
-                  self.bucket_size) % self.num_of_bucket
+                  self.sampling_interval) % self.num_of_bucket
         self.buckets[seq] += 1
 
     def show(self):
